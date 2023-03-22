92783420f31dba6d0bd8b4d194dbbd1c9ec04f20
sjamescarter <stephenjamescarter@gmail.com>
2023-03-22 13:38:15 -0500

    commit tree b952d1a8382949853c288ea56f51f47707d66e8e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,20 @@
 class Triangle
-  # write code here
+  attr_reader :kind
+
+  def initialize(one, two, three)
+    triangle = [one, two, three]
+    if one + two <= three || two + three <= one || one + three <= two || triangle.sum == 0
+      raise TriangleError
+    elsif triangle.uniq.count == 1
+      @kind = :equilateral
+    elsif triangle.uniq.count == 2
+      @kind = :isosceles
+    elsif
+      @kind = :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    "Not a triangle"
+  end
 end
